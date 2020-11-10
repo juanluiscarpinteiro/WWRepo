a054017407081c45d31aa288ba9b84c0b365ca69
Juan Carpinteiro <juanluiscarpinteiro@users.noreply.github.com>
2020-11-10 16:05:38 -0500

    formatted properly question3

--- question3.rb
+++ question3.rb
@@ -1,15 +1,16 @@
 def nthSmallestNumber(number, arrayValues)
     arrayLocal =arrayValues
     arrayLocal = arrayLocal.sort
-    puts "Array Info"
-    puts arrayLocal
+    # for debug and show porpuses uncomment the two lines below
+    # puts "Array Info"
+    # puts arrayLocal
     return arrayLocal[number-1]
 end
 
 
     randomNumbers = Array.new(500) { rand(500) }
-    puts "The nth smallest number is " 
-    puts nthSmallestNumber(5,randomNumbers)
+    puts "The nth smallest number out of 500 numbers is " 
+    puts  nthSmallestNumber(5,randomNumbers)
 
 
  
